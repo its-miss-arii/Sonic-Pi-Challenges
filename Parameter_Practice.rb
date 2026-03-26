93971f5d29e8f78b5519eb53c95c7497749d2003
its-miss-arii <arianna.m.rodriguez0809@gmail.com>
2026-03-26 10:25:42 -0700

    Create Parameter_Practice.rb

---
+++ Parameter_Practice.rb
@@ -0,0 +1,16 @@
+#inefficient_grades.rb
+
+# the function below accepts no parameter (inputs)
+define :studentQuizScore do |name, score|
+  print("STUDENT NAME: ")
+  print(name)
+  print("QUIZ SCORE: ")
+  print(score)
+end
+
+#call function
+studentQuizScore "Eric", 2
+studentQuizScore "Ty", 3
+studentQuizScore "Angela", 4
+studentQuizScore "Sandy", 3.5
+studentQuizScore "Quinne", 1.5
