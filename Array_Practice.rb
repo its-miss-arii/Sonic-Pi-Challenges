4ba2e659ae6a454dcb0b214d8ef539533997ec19
its-miss-arii <arianna.m.rodriguez0809@gmail.com>
2026-03-26 10:24:50 -0700

    Create Array_Practice.rb

---
+++ Array_Practice.rb
@@ -0,0 +1,15 @@
+#inefficient_alphabet.rb
+
+# the array containing all the letters has been created for you!
+Letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+i = 0
+
+# DIRECTIONS: modify the code below so it uses the array and a loop!
+26.times do
+  print(Letters[i])
+  sleep(0.25)
+  i = i + 1
+  end
+  
+  print("")
+  print("Now I know that you know that I know my ABCs")
